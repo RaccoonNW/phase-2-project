14fe11509365c08fd4311d6647ea7cbd21a78330
Gabriel <shalitmontagne.g@gmail.com>
2022-01-11 17:28:17 -0700

    add fetch functions and returns to Options.js

---
+++ phase-2-project/src/Components/Options.js
@@ -0,0 +1,42 @@
+import './App.css'
+
+function Options({setDifficulty, setSolution}) {
+
+    function handleSetDifficulty() {
+        const element = document.getElementById('difficulty-select')
+        if (element.value == 'easy') {
+            fetch('http://localhost:3001/puzzles')
+                .then((r) => r.json())
+                .then((puzzles) => setDifficulty(puzzles[0]))
+        }
+        else if (element.value == 'medium') {
+            fetch('http://localhost:3001/puzzles')
+                .then((r) => r.json())
+                .then((puzzles) => setDifficulty(puzzles[1]))
+        }
+        else {
+            fetch('http://localhost:3001/puzzles')
+                .then((r) => r.json())
+                .then((puzzles) => setDifficulty(puzzles[2]))
+        }
+    }
+    
+        
+
+    return (
+        <div id="setup-game">
+            <div id="difficulty">
+                <h3>Choose Difficulty:</h3>
+                <select name='difficulty' id='difficulty-select' onChange={handleSetDifficulty}>  
+                    <option value=''>Select Difficulty</option>
+                    <option id='diff-1' value='easy'>Easy</option>
+                    <option id='diff-2' value='medium'>Medium</option>
+                    <option id='diff-3' value='hard'>Hard</option>
+                </select>
+            </div>
+        </div>
+    )
+
+}
+
+export default Options
