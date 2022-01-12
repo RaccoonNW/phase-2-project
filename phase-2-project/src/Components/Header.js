ba655063863ad41dd539b6cde94e4afd42597e38
Gabriel <shalitmontagne.g@gmail.com>
2022-01-11 17:27:33 -0700

    add content to Header.js

---
+++ phase-2-project/src/Components/Header.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function Header () {
+
+    return (
+            <div id="header">
+                <div id="title">
+                    <h1>Sudoku</h1>
+                    <h5 id="subtitle">In React</h5>
+                </div>
+            </div>
+    )
+}
+
+export default Header
